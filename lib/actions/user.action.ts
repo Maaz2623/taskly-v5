79603462ba538334bb14e675e698ff1dd314d56b
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-07-05 18:56:45 +0530

    modified:   lib/actions/user.action.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,6 +6,7 @@ import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 import { stringify } from "querystring";
+import { CreateUserParams } from "@/types";
 
 // Create
 export async function createUser(user: CreateUserParams) {
